b679e5dff8522ce157b971b271c52f0388cf545d
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-06-18 12:09:02 -0300

    FEAT: Adequações porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
-import Porta from "../components/Porta";
+import Porta from '../components/Porta'
+import PortaModel from "../components/Porta";
 
 export default function Home() {
+  const p1 = new PortaModel(1, false, true)
+  const p2 = new PortaModel(2, false, )
   return (
     <div style={{ display: "flex" }}>
-      <Porta selecionada />
-      <Porta />
+      <Porta porta={p1} />
+      <Porta porta={p2} />
     </div>
   )
 }
